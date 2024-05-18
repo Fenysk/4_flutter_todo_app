c8d2131d8915442b16c8891ca67d8abe48115548
Fenysk <minefenysk@gmail.com>
2024-05-18 12:42:49 +0200

    🎉 init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Todo App'),
+        ),
+        body: const Center(
+          child: Text('Hello world !'),
+        ),
+      ),
+    );
+  }
+}
