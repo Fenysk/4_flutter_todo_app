af8a46468c5e32d2b8d568545d4bef9b32599765
Fenysk <minefenysk@gmail.com>
2024-05-19 15:38:46 +0200

    👔  TodoList to use ListView.builder and add Dismissible

--- lib/widgets/todo_list.dart
+++ lib/widgets/todo_list.dart
@@ -3,27 +3,26 @@ import 'package:todo_app/models/todo.dart';
 import 'package:todo_app/widgets/todo_item.dart';
 
 class TodoList extends StatelessWidget {
-  const TodoList(this.todos, {super.key, required this.onToggleDone});
+  const TodoList({super.key, required this.todos, required this.onToggleDone});
 
   final List<Todo> todos;
   final void Function(String id) onToggleDone;
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          final todo = todos[index];
-          return Dismissible(
-            key: ValueKey(todo.id),
-            onDismissed: (direction) => onToggleDone(todo.id),
-            child: TodoItem(
-              todo: todo,
-            ),
-          );
-        },
-      ),
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: todos.length,
+      itemBuilder: (context, index) {
+        final todo = todos[index];
+        return Dismissible(
+          key: ValueKey(todo.id),
+          onDismissed: (direction) => onToggleDone(todo.id),
+          child: TodoItem(
+            todo: todo,
+          ),
+        );
+      },
     );
   }
 }
